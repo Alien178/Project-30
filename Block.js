f8ebd4dfd5b13a5eb4df61387e1d1b1969e183c1
Alien178 <57078903+Alien178@users.noreply.github.com>
2020-09-16 17:54:58 -0700

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,36 @@
+class Block {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction': 1.0,
+        'density': 1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.visibility = 255;
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    if (this.body.position.y >= 560) {
+      push();
+      World.remove(world, this.body);
+      this.visibility = this.visibility - 5;
+      tint(255, this.visibility);
+      pop();
+    } else {
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(2);
+      stroke(217, 82, 4);
+      fill(242, 135, 5);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  }
+}
---
+++ Polygon.js
@@ -0,0 +1,33 @@
+class Polygon {
+    constructor(x, y, radius, npoints) {
+        var options ={
+            density : 1.5,
+            restitution: 0.5,
+            friction: 1.0
+        }
+
+        this.body =  Bodies.polygon(x, y, radius, npoints * 4, options);
+        this.radius = radius;
+        this.npoints = npoints;
+        World.add(world, this.body);
+    }
+
+    display() {
+        var pos = this.body.position;
+        //pos.x = mouseX;
+        //pos.y = mouseY;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        strokeWeight(4);
+        stroke(64, 0, 0);
+        fill(127, 0, 0);
+        //scale(5);
+        polygon(0, 0, this.radius, this.npoints);
+        //polygon(0, 0, 200, 6);
+        pop();
+    }
+
+}
